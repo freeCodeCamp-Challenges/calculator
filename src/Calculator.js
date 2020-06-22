bbc92075fc7a9e1f586938b6e27d3a7904225ca7
fotis tsakiris <footios76@gmail.com>
2020-06-22 07:57:18 +0300

    try to fix userStory but many bugs

--- src/Calculator.js
+++ src/Calculator.js
@@ -111,66 +111,162 @@ const Calculator = props => {
   //   setNumber(number.split(' ').slice(0,1).join())
   // }
 
-  const doTheMath = () => {
-    console.log("doTheMath number ", number);
-    console.log("doTheMath storedNumber ", storedNumber);
-    // console.log("functionType", functionType);
-    switch (functionType) {
-      case "add":
-        setNumber(
-          Math.round((parseFloat(storedNumber) + parseFloat(number)) * 100) /
-            100
-        );
-        setStoredNumber(0);
-        // setHistory("");
-        break;
-      case "subtract":
-        // If user previously pressed multiply or divide and then the minus sign,
-        // take the minus number and do multiplicatioin / division
-        // instead of subtraction.
-        if (previousFunctionType === "multiply") {
-          setNumber(
-            Math.round(
-              parseFloat(storedNumber) * -Math.abs(parseFloat(number)) * 1000
-            ) / 1000
-          );
-        } else if (previousFunctionType === "divide") {
-          setNumber(
-            Math.round(
-              ((parseFloat(storedNumber) / -Math.abs(parseFloat(number))) * 1000
-            ) / 1000).toFixed(4) 
-          );
-        } else {
-          setNumber(
-            (Math.round((parseFloat(storedNumber) - parseFloat(number)) * 1000) /
-              1000).toFixed(4) 
-          );
-        }
-        setStoredNumber(0);
-        // setHistory("");
-        break;
-      case "divide":
-        // setStoredNumber(number);
-        setStoredNumber("0");
-        setNumber(
-          (Math.round((parseFloat(storedNumber) / parseFloat(number)) * 1000) /
-            1000).toFixed(4) 
-        );
-        // console.log(' divide', number, storedNumber );
-        // setHistory("");
-        break;
-      case "multiply":
-        setNumber(
-          Math.round(parseFloat(storedNumber) * parseFloat(number) * 1000) /
-            1000
-        );
-        setStoredNumber(0);
-        // setHistory("");
-        break;
-      default:
-        break;
-    }
-  };
+  const doTheMath = useCallback(
+    newNumber => {
+      let type = functionType;
+      // In case user makes caclulations one after the other,
+      // without preesing the equals button.
+      // if (!!newNumber) {
+      //   console.log("doTheMath number >= 0...");
+      //   type = previousFunctionType;
+      // }
+      console.log("newNumber", newNumber);
+      console.log("doTheMath number ", number);
+      console.log("doTheMath storedNumber ", storedNumber);
+      console.log("doTheMath functionType", functionType);
+      console.log("doTheMath previousFunctionType", previousFunctionType);
+      switch (type) {
+        case "add":
+          // In case user makes caclulations one after the other,
+          // without preesing the equals button.
+          if (!!newNumber) {
+            console.log("doTheMath number >= 0...");
+            setStoredNumber(
+              Math.round((parseFloat(number) + parseFloat(newNumber)) * 100) /
+                100
+            );
+            setNumber("0");
+          } else if (!newNumber) {
+            setNumber(
+              Math.round(
+                (parseFloat(storedNumber) + parseFloat(number)) * 100
+              ) / 100
+            );
+            setStoredNumber(0);
+          }
+
+          // setHistory("");
+          break;
+        case "subtract":
+          // In case user makes caclulations one after the other,
+          // without presing the equals button.
+          if (!!newNumber) {
+            // If user previously pressed multiply or divide and then the minus sign,
+            // take the minus number and do multiplication / division
+            // instead of subtraction.
+            if (previousFunctionType === "multiply") {
+              setStoredNumber(
+                Math.round(
+                  parseFloat(number) * -Math.abs(parseFloat(newNumber)) * 1000
+                ) / 1000
+              );
+            } else if (previousFunctionType === "divide") {
+              setStoredNumber(
+                Math.round(
+                  ((parseFloat(number) / -Math.abs(parseFloat(newNumber))) *
+                    1000) /
+                    1000
+                ).toFixed(4)
+              );
+            } else {
+              setStoredNumber(
+                (
+                  Math.round(
+                    (parseFloat(number) - parseFloat(newNumber)) * 1000
+                  ) / 1000
+                ).toFixed(4)
+              );
+            }
+            setNumber("0");
+            // setHistory("");
+          } else if (!newNumber) {
+            // If user previously pressed multiply or divide and then the minus sign,
+            // take the minus number and do multiplication / division
+            // instead of subtraction.
+            if (previousFunctionType === "multiply") {
+              setNumber(
+                Math.round(
+                  parseFloat(storedNumber) *
+                    -Math.abs(parseFloat(number)) *
+                    1000
+                ) / 1000
+              );
+            } else if (previousFunctionType === "divide") {
+              setNumber(
+                Math.round(
+                  ((parseFloat(storedNumber) / -Math.abs(parseFloat(number))) *
+                    1000) /
+                    1000
+                ).toFixed(4)
+              );
+            } else {
+              setNumber(
+                (
+                  Math.round(
+                    (parseFloat(storedNumber) - parseFloat(number)) * 1000
+                  ) / 1000
+                ).toFixed(4)
+              );
+            }
+            setStoredNumber(0);
+            // setHistory("");
+          }
+
+          break;
+        case "divide":
+          // In case user makes caclulations one after the other,
+          // without presing the equals button.
+          if (!!newNumber) {
+            console.log("doTheMath multiply number >= 0...");
+            setStoredNumber(
+              Math.round((parseFloat(number) / parseFloat(newNumber)) * 1000) /
+                1000
+            ).toFixed(4);
+            setNumber("0");
+          } else if (!newNumber) {
+            setNumber(
+              (
+                Math.round(
+                  (parseFloat(storedNumber) / parseFloat(number)) * 1000
+                ) / 1000
+              ).toFixed(4)
+            );
+            setStoredNumber(0);
+            // setHistory("");
+          }
+          break;
+        case "multiply":
+          // In case user makes caclulations one after the other,
+          // without preesing the equals button.
+          if (!!newNumber) {
+            console.log("doTheMath multiply number >= 0...");
+            setStoredNumber(
+              Math.round(parseFloat(number) * parseFloat(newNumber) * 1000) /
+                1000
+            );
+            setNumber("0");
+          } else if (!newNumber) {
+            setNumber(
+              Math.round(parseFloat(storedNumber) * parseFloat(number) * 1000) /
+                1000
+            );
+            setStoredNumber(0);
+            // setHistory("");
+          }
+          break;
+        default:
+          break;
+      }
+    },
+    [
+      number,
+      storedNumber,
+      functionType,
+      setNumber,
+      setStoredNumber,
+      previousFunctionType
+    ]
+  );
   // , [number, storedNumber, setStoredNumber, setNumber, functionType]);
 
   const handleClick = useCallback(
@@ -199,6 +295,17 @@ const Calculator = props => {
 
             break;
           case "one":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(1);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-1");
@@ -208,6 +315,34 @@ const Calculator = props => {
             // setHistory(history => history + 1);
             break;
           case "two":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              console.log('storedNumber === 0 ');
+              
+              doTheMath(2);
+              break;
+            }
+
+            if (
+              storedNumber >= 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              console.log('storedNumber >= 0');
+              ;
+              doTheMath(2);
+              break;
+            }
+
+
+
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-2");
@@ -217,6 +352,17 @@ const Calculator = props => {
             // setHistory(history => history + number + 2);
             break;
           case "three":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(3);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-3");
@@ -226,6 +372,17 @@ const Calculator = props => {
             // setHistory(history => history + number + 3);
             break;
           case "four":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(4);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-4");
@@ -235,6 +392,17 @@ const Calculator = props => {
             // setHistory(history => history + number + 4);
             break;
           case "five":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(5);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-5");
@@ -244,6 +412,17 @@ const Calculator = props => {
             // setHistory(history => history + number + 5);
             break;
           case "six":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(6);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-6");
@@ -253,6 +432,17 @@ const Calculator = props => {
             // setHistory(history => history + number + 6);
             break;
           case "seven":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(7);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-7");
@@ -262,6 +452,17 @@ const Calculator = props => {
             // setHistory(history => history + number + 7);
             break;
           case "eight":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(8);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-8");
@@ -271,6 +472,17 @@ const Calculator = props => {
             // setHistory(history => history + number + 8);
             break;
           case "nine":
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            if (
+              storedNumber === 0 &&
+              +number >= 0 &&
+              !!previousFunctionType &&
+              !!functionType
+            ) {
+              doTheMath(9);
+              break;
+            }
             // Set number to minus, if user pressed '-' before number.
             if (functionType === "subtract" && !previousFunctionType === " ") {
               setNumber("-9");
@@ -295,13 +507,14 @@ const Calculator = props => {
         setFunctionType(id);
         switch (id) {
           case "add":
+            setPreviousFunctionType(functionType);
             console.log("add", id, storedNumber, number);
 
-            if (number === undefined) {
-              setNumber(" ");
-              break;
-            }
-            setNumber(" ");
+            // if (number === undefined) {
+            //   setNumber("0");
+            //   break;
+            // }
+            setNumber("0");
             // setHistory(history => history + " + ");
             // console.log("add", number, storedNumber);
 
@@ -317,11 +530,14 @@ const Calculator = props => {
             console.log("subtract number", number);
             console.log("subtract storedNumber ", storedNumber);
 
-            if (number == " " && storedNumber >= 0) {
-              console.log('number === " " ', number === " ");
+            if (+number === 0 && storedNumber >= 0) {
+              console.log('+number == 0 ');
+              break;
+            } else if (+number >= 0 && storedNumber >= 0) {
+              console.log('+number >= 0 ');
               break;
             } else {
-              setNumber(" ");
+              setNumber("0");
               // setHistory(history => history + " - ");
               setStoredNumber(
                 Math.round(
@@ -332,10 +548,11 @@ const Calculator = props => {
 
             break;
           case "divide":
-            if (number === undefined) {
-              break;
-            }
-            setNumber(" ");
+            setPreviousFunctionType(functionType);
+            // if (number === undefined) {
+            //   break;
+            // }
+            // setNumber("0");
             // setHistory(history => history + " / ");
 
             // eslint-disable-next-line
@@ -347,15 +564,27 @@ const Calculator = props => {
 
             break;
           case "multiply":
+            setPreviousFunctionType(functionType);
             // setHistory(history => history + " * ");
-            console.log("multiply", id, storedNumber, number);
+            console.log("multiply", id, functionType, storedNumber, number);
             // In case user presses two fuction types (=,-,?,*)
             // one after the other. Get the last one
-            if (number == " " && storedNumber >= 0) {
+            if (+number === 0 && storedNumber >= 0) {
               console.log("number === undefined", number === undefined);
               break;
             }
-            setNumber(" ");
+            // In case user makes caclulations one after the other,
+            // without preesing the equals button.
+            else if (+number >= 0 && storedNumber >= 0) {
+              console.log(
+                "number >= 0 && storedNumber >= 0",
+                number >= 0 && storedNumber >= 0
+              );
+              setNumber("0");
+              doTheMath();
+              break;
+            }
+            setNumber("0");
             // eslint-disable-next-line
             if (storedNumber != 0 && !isNaN(number)) {
               setStoredNumber(parseFloat(storedNumber) * parseFloat(number));
@@ -372,7 +601,7 @@ const Calculator = props => {
             }
             break;
           case "clear":
-            setNumber("");
+            setNumber("0");
             setStoredNumber(0);
             setPreviousFunctionType("");
             setFunctionType("");
@@ -383,7 +612,15 @@ const Calculator = props => {
         }
       }
     },
-    [number, setNumber, storedNumber, setStoredNumber, doTheMath]
+    [
+      number,
+      setNumber,
+      storedNumber,
+      setStoredNumber,
+      doTheMath,
+      functionType,
+      previousFunctionType
+    ]
   );
   console.log("render ......................");
 
