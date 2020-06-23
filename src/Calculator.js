782cc3890f98485a7ab1560ed8946cef35b5182f
fotis tsakiris <footios76@gmail.com>
2020-06-23 11:08:19 +0300

    user story 14, user continues with caclulations

--- src/Calculator.js
+++ src/Calculator.js
@@ -95,35 +95,39 @@ const Calculator = props => {
   const [operators, setOperators] = useState([]);
   const [number, setNumber] = useState("0");
   const [elements, setElements] = useState([]);
+  const [equalsSign, setEqualsSign] = useState([]);
 
   console.log("OUT // insertedNumbers ", insertedNumbers);
   console.log("OUT // operators ", operators);
   console.log("OUT // elements ", elements);
+  console.log("OUT // equalsSign ", equalsSign);
 
   const doTheMath = useCallback(() => {
     console.log("DO-MATH");
+    console.log("DO-MATH equalsSign ", equalsSign);
 
     let numbers = [...insertedNumbers];
     let opers = [...operators];
     let elems = [...elements];
     // Get also the last number and operator.
     numbers.push(number);
-    opers.push(operators);
+    opers.push(...operators);
     elems.push(number); // a number is the last element.
 
     const makeCalculations = () => {
       numbers.map((num, i) => {
         let result = 0;
-        const firstNum = numbers[i];
-        const secondNum = numbers[i + 1];
+        let firstNum = numbers[i];
+        let secondNum = numbers[i + 1];
         // If the are no more numbers, give the firstNum as the result.
         if (secondNum === undefined) {
           setNumber(firstNum);
           return true;
         }
+        // setNumber(numbers[i]);
         console.log("numbers", numbers);
         console.log("numbers[i]", numbers[i]);
-        console.log("elements", elements);
+        console.log("elems", elems);
         console.log("elems[i]", elems[i]);
         console.log("opers", opers);
         console.log("opers[i]", opers[i]);
@@ -134,12 +138,34 @@ const Calculator = props => {
           isNaN(elems[i + 2]),
           isNaN(elems[i + 3])
         );
-
+        // If user presses 3 operators in a row,
+        // take the last elment of the operators.
         if (isNaN(elems[i + 1]) && isNaN(elems[i + 2]) && isNaN(elems[i + 3])) {
-          console.log("operIndex");
-          operIndex =+  2; // 0, 1, 2
+          // console.log("operIndex");
+          operIndex = +2; // 0, 1, 2
         }
-        console.log("opers[i + operIndex]", opers[operIndex]);
+        // If user pressed the equals and continued calculations.
+        if (equalsSign[0] === '=') {
+          console.log(
+            "// dothemath if (!equalsSign)",
+            'first', numbers[numbers.length - 2], 
+            'secondNum',  numbers[numbers.length - 1],
+            'opers', opers,
+            'operIndex', opers.length - 1
+          );
+          firstNum = numbers[numbers.length - 2];
+          secondNum = numbers[numbers.length - 1];
+          operIndex = opers.length - 1
+        }
+
+       
+
+        console.log(
+          "opers[i + operIndex]",
+          opers[operIndex],
+          "operIndex",
+          operIndex
+        );
         switch (opers[operIndex]) {
           case "+":
             if (elems[i + 1] === "+" && elems[i + 2] === "-") {
@@ -166,6 +192,12 @@ const Calculator = props => {
             if (elems[i + 1] === "/" && elems[i + 2] === "-") {
               result = +firstNum / -+secondNum;
             } else {
+              console.log(
+                "In / result = +firstNum / +secondNum; ",
+                result,
+                firstNum,
+                secondNum
+              );
               result = +firstNum / +secondNum;
             }
             break;
@@ -176,16 +208,27 @@ const Calculator = props => {
         opers.splice(0, 1);
         // Put the result as the firstNum.
         numbers.unshift(result);
-        // setNumber(result);
-        makeCalculations();
 
+        // If user pressed the equals and continued calculations.
+        if (equalsSign[0] === '=') {
+          console.log('equalsSign[0] === ');
+          setNumber(result);
+          return true
+        }
+        
+        makeCalculations();
+        // setEqualsSign(false); // in case user coninues after getting a result
+        
+        
         return true;
       });
+      
     };
 
-    // console.log("numbers ", numbers, "operators ", operators);
+    console.log("elements[elements.length - 1] ", elements[elements.length - 1]);
 
     makeCalculations();
+    elems.push(elements[elements.length - 1]);
   }, [insertedNumbers, number, operators, elements]);
 
   const handleClick = useCallback(
@@ -193,9 +236,13 @@ const Calculator = props => {
       const removeInitialZero = () => (number === "0" ? setNumber("") : null);
 
       const gatherElements = () => {
-        console.log("///gatherElements", number);
+        if (sign === "=") {
+          doTheMath();
+        }
+
+        console.log("///gatherElements", number, sign);
         if (sign === "+" || sign === "-" || sign === "*" || sign === "/") {
-          console.log('if (sign === "+"....', sign);
+          console.log(' gatherElements if (sign === "+"....', sign);
           setOperators([...operators, sign]);
           setElements([...insertedNumbers, ...operators, sign]);
         }
@@ -204,23 +251,30 @@ const Calculator = props => {
           setElements([...elements, number]);
         }
 
-        if (sign === "=") {
-          doTheMath();
-        }
+       
       };
 
       if (!isNaN(+sign)) {
-        console.log("!isNaN(+sign) elements", elements);
+        console.log("It is a number = elements......", elements);
 
         removeInitialZero();
         setNumber(num => num + sign);
       } else if (sign === "+" || sign === "-" || sign === "*" || sign === "/") {
-        console.log("gatherElements - setNumber", sign, number);
+        console.log(
+          "handlelclick if (sign === '='....- sign, number",
+          sign,
+          number
+        );
+
         gatherElements();
         setNumber("0");
       } else {
         switch (sign) {
           case "=":
+            setEqualsSign([...equalsSign, sign]);
+            console.log('In = elements', elements, " sign", sign);
+
+            // setElements([...elements, ...operators, sign])
             gatherElements();
             break;
           case ".":
@@ -233,6 +287,8 @@ const Calculator = props => {
             setInsertedNumbers([]);
             setOperators([]);
             setElements([]);
+            setEqualsSign([])
+            // setEqualsSign(false);
             break;
           default:
             break;
